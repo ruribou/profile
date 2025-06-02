50994a68bbecc2f76a0c9e6b7ec6fd73c352f8cf
ruribou <ruribou@hotmail.com>
2025-06-03 00:05:30 +0900

    諸々の修正

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -30,26 +30,6 @@ const AboutSection = () => (
         趣味はアイドルマスター、写真撮影、プロ野球観戦です。<span className="hidden sm:inline"><br/></span>
         新しい技術を学ぶことが好きです。
       </p>
-      <div className="flex justify-center space-x-6 pt-4">
-        <a 
-          href="https://x.com/ryosan177"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center space-x-2 bg-blue-800 hover:bg-blue-700 px-4 py-2 rounded-full transition duration-300"
-        >
-          <SiX className="w-5 h-5" />
-          <span>@ryosan177</span>
-        </a>
-        <a
-          href="https://github.com/ruribou"
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="flex items-center space-x-2 bg-blue-800 hover:bg-blue-700 px-4 py-2 rounded-full transition duration-300"
-        >
-          <SiGithub className="w-5 h-5" />
-          <span>@ruribou</span>
-        </a>
-      </div>
     </div>
   </section>
 );
