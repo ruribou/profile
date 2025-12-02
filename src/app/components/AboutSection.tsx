cbe3d153ea68b973339340b4dcfb03250d7aceb3
ruribou <ruribou@hotmail.com>
2025-12-02 15:54:54 +0900

    fix: 諸々修正

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -10,7 +10,7 @@ const AboutSection = () => (
         className="w-32 h-32 rounded-full mx-auto shadow-lg object-cover"
       />
       <div>
-        <h1 className="text-5xl font-bold mb-4">りょうさん</h1>
+        <h1 className="text-5xl font-bold mb-4">Ryota Asai</h1>
         <div className="flex items-center justify-center space-x-8 text-xl text-blue-100 py-2 mb-4">
           <div className="flex items-center space-x-2">
             <FiMapPin className="w-5 h-5" />
@@ -18,21 +18,20 @@ const AboutSection = () => (
           </div>
           <div className="flex items-center space-x-2">
             <FiBriefcase className="w-5 h-5" />
-            <span className="text-base sm:text-xl font-bold text-white">学生エンジニア</span>
+            <span className="text-base sm:text-xl font-bold text-white">学生+ソフトウェアエンジニア</span>
           </div>
         </div>
       </div>
       <p className="mx-auto text-sm sm:text-lg text-white max-w-xs sm:max-w-2xl">
-        埼玉の山奥で大学生をしています。Rubyistです。<span className="hidden sm:inline"><br/></span>
+        埼玉の山奥で大学生をしています。<span className="hidden sm:inline"><br/></span>
         技術コミュニティの運営、NPO法人の運営もしています。<span className="hidden sm:inline"><br/></span>
         <span className="font-bold underline">技術は目的ではなく、手段である</span>ということを大切にしています。
       </p>
       <p className="mx-auto text-sm sm:text-lg text-white max-w-xs sm:max-w-2xl">
         趣味はアイドルマスター、写真撮影、プロ野球観戦などです。<span className="hidden sm:inline"><br/></span>
-        横浜DeNAベイスターズと市川雛菜が好きです。
       </p>
     </div>
   </section>
 );
 
-export default AboutSection; 
+export default AboutSection;
